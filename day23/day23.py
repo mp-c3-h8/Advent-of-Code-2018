caff26f1aaa71c6507e28dd245c341756af9f028
mp <no@e.mail>
2026-03-04 02:10:36 +0100

    day23

--- day23/day23.py
+++ day23/day23.py
@@ -2,6 +2,7 @@ import os.path
 import os
 import sys
 import re
+from z3 import Int, If, Abs, Optimize, sat
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
 sys.path.append(os.path.join(dir_path, '..', 'utils'))
@@ -32,6 +33,21 @@ def strongest_nanobot(nanobots: list[NanoBot]) -> int:
     return res
 
 
+def best_position(nanobots: list[NanoBot]) -> tuple[int, int]:
+    solver = Optimize()
+    X, Y, Z, R, D = Int("X"), Int("Y"), Int("Z"), Int("R"), Int("D")
+    objective = X*0  # dummy to start
+    for (x, y, z), r in nanobots:
+        objective += If(Abs(X-x)+Abs(Y-y)+Abs(Z-z) <= r, 1, 0)  # type: ignore
+    solver.add(R == objective)
+    solver.add(D == Abs(X)+Abs(Y)+Abs(Z))  # type: ignore
+    solver.maximize(R)  # max number of bots in range
+    solver.minimize(D)  # min distance to origin
+    assert solver.check() == sat, "Z3 could not solve :("
+    model = solver.model()
+    return model[R].as_long(), model[D].as_long()  # type: ignore
+
+
 @timed("All")
 def main() -> None:
     input_path = os.path.join(dir_path, "input.txt")
@@ -41,6 +57,9 @@ def main() -> None:
     nanobots = create_nanobots(data)
     p1 = strongest_nanobot(nanobots)
     print("Part 1:", p1)
+
+    num_bots, dist = best_position(nanobots)
+    print(f"Part 2: {dist} ({num_bots} bots in range)")
 
 
 if __name__ == "__main__":
